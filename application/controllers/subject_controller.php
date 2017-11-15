81fbbaf806b126724dc60b9c7f762ba54c85d68d
dustineviado <dustineviado@gmail.com>
2017-11-15 20:21:06 +0800

    11/15/17 8:21 PM

--- application/controllers/subject_controller.php
+++ application/controllers/subject_controller.php
@@ -3,18 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class subject_controller extends CI_Controller {
 
-	public function index()
-	{
+	public function __construct(){
+       
+        parent::__construct();
+        $this->load->model('subject_model');
+        $this->load->helper('url_helper');
+    }
+
+	public function index(){
+
 		$data['title'] = "Subjects | Ramon Magsaysay High School";
+		$data['subjects'] = $this->subject_model->selectsubjects();
 		
+
 		$this->load->view('templates/header',$data);
 		$this->load->view('vthesis/subject',$data);
 		$this->load->view('templates/footer',$data);
+
 	}
-
-	
-
 }
-
 /* End of file  subject_controller.php */
 /* Location: ./application/controllers/ subject_controller.php */
--- application/views/vthesis/subject.php
+++ application/views/vthesis/subject.php
@@ -4,38 +4,40 @@
 		</div>
 		<div class="col-lg-10">
 			<br>
-			<h1 class="subjectfont">Subjects</h1>
-				<form class="form-row">
-					<select class="form-control col-auto">
-						<option selected>Subjects Level</option>
-  						<option value="1">Kinder</option>
-  						<option value="2">Preaparatory</option>
-  						<option value="3">Grade1</option>
-  						<option value="3">Grade2</option>
-  						<option value="3">Grade3</option>
-  						<option value="3">Grade4</option>
-  						<option value="3">Grade5</option>
-  						<option value="3">Grade6</option>
-  						<option value="3">Grade7</option>
-  						<option value="3">Grade8</option>
-  						<option value="3">Grade9</option>
-  						<option value="3">Grade10</option>
-  						<option value="3">Grade11</option>
-  						<option value="3">Grade12</option>
+				<br>
+				<br>
+				<div class="container">
+					<h1 class="subjectfont">Subjects</h1>
+					<form class="form-row">
+					<select class="form-control col-auto selectmargin" name="selsub" id="pangalan">
+							<option value="all">All Levels</option>
+							<option value="Kinder">Kinder</option>
+							<option value="Preparatory">Preparatory</option>
+							<option value="Grade 1">Grade 1</option>
+							<option value="Grade 2">Grade 2</option>
+							<option value="Grade 3">Grade 3</option>
+							<option value="Grade 4">Grade 4</option>
+							<option value="Grade 5">Grade 5</option>
+							<option value="Grade 6">Grade 6</option>
+							<option value="Grade 7">Grade 7</option>
+							<option value="Grade 8">Grade 8</option>
+							<option value="Grade 9">Grade 9</option>
+							<option value="Grade 10">Grade 10</option>
+							<option value="Grade 11">Grade 11</option>
+							<option value="Grade 12">Grade 12</option>
 					</select>
-				</form>
-
-				<form class="form-row marginsrch">
+						<script type="text/javascript">
+	   						document.getElementById('pangalan').value = "<?php echo $_GET['selsub'];?>";
+						</script>
+					&nbsp &nbsp
 					<input type="text" class="form-control col-auto" placeholder="Search...">
 					<button class="input-group-addon btn searchbtn"> <span class="fa fa-search searchfont"></span> </button>
-				</form>
-
+				</form>	
 				<br>
 				<br>
 
-				<div class="container">
 					<div>
-						<a class="btn addsubbtn" role="button" href="add_subject_controller">Add a Subject</a>
+						<button id="addmodalbtn" class="btn addsubbtn" data-toggle="modal" data-target="#addmodal">Add Subject</button>
 					</div>
 
 					<table class="table table-responsive table-striped">
@@ -49,23 +51,86 @@
 							</tr>
 						</thead>
 						<tbody>
+						<?php if ($subjects != null): ?>
+						<?php foreach($subjects as $sub): ?>
 							<tr>
-								<td>1738</td>
-								<td>Algebra</td>
-								<td>Math</td>
-								<td>Grade 10</td>
-								<td>View | Edit</td>
-							</tr>
-							<tr>
-								<td>1590</td>
-								<td>Filipino Literature</td>
-								<td>English</td>
-								<td>Grade 7</td>
-								<td>View | Edit</td>
+								<td><?php echo $sub['subid']; ?></td>
+								<td><?php echo $sub['subject']; ?></td>
+								<td><?php echo $sub['faculty']; ?></td>
+								<td><?php echo $sub['year_level']; ?></td>
+								<td>Delete | Edit</td>
 							</tr>
 						</tbody>
+						<?php endforeach; ?>
+						<?php else: echo 'Denied' ?>
+						<?php print_r($subjects) ?>
+						<?php endif; ?>
 					</table>
+				
 				</div>
+
+				<div class="modal fade" id="addmodal" tabindex="-1" role="dialog" aria-labelledby="subjectmodal" aria-hidden="true">
+				  <div class="modal-dialog modal-lg" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <h1 class="modal-title" id="subjectmodal"><b>Add Subject</b></h1>
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				          <span aria-hidden="true">&times;</span>
+				        </button>
+				      </div>
+				      <div class="modal-body">
+				        <form>
+								<div class="row form-group">
+									<div class="col-md">
+										<label for="subjid" class="col-form-label formmodalfont">Subject ID</label>
+										<input id="subjid" name="subjectidname" type="text" class="form-control" placeholder="Subject ID">
+									</div>
+									<div class="col-md">
+										<label for="subjname" class="col-form-label formmodalfont">Subject Name</label>
+										<input id="subjname" name="subjectname" type="text" class="form-control" placeholder="Subject Name">
+									</div>
+									<div class="col-md">
+										<label for="subjfac" class="col-form-label formmodalfont">Subject Faculty</label>
+										<input id="subjfac" name="subjectfaculty" type="text" class="form-control" placeholder="Subject Faculty">
+									</div>
+									<div class="col-md">
+										<label for="subjlevel" class="col-form-label formmodalfont">Level</label>
+										<input id="subjlevel" name="subjectlevel" type="text" class="form-control" placeholder="Level">
+									</div>
+								</div> 
+				      </div>
+				      <div class="modal-footer">
+				       <button type="button" class="btn addsubbtn2">Add Subject</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+
+						<!-- <h1><b>Add Subject<b></h1>
+							<br>
+							 <form>
+								<div class="row form-group">
+									<div class="col-md">
+										<label for="subjid" class="col-form-label formmodalfont">Subject ID</label>
+										<input id="subjid" name="subjectidname" type="text" class="form-control" placeholder="Subject ID">
+									</div>
+									<div class="col-md">
+										<label for="subjname" class="col-form-label formmodalfont">Subject Name</label>
+										<input id="subjname" name="subjectname" type="text" class="form-control" placeholder="Subject Name">
+									</div>
+									<div class="col-md">
+										<label for="subjfac" class="col-form-label formmodalfont">Subject Faculty</label>
+										<input id="subjfac" name="subjectfaculty" type="text" class="form-control" placeholder="Subject Faculty">
+									</div>
+									<div class="col-md">
+										<label for="subjlevel" class="col-form-label formmodalfont">Level</label>
+										<input id="subjlevel" name="subjectlevel" type="text" class="form-control" placeholder="Level">
+									</div>
+								</div> 
+
+								<div>
+									<button class="btn addsubbtn2">Add Subject</button>
+								</div> -->
 
 		</div>
 
